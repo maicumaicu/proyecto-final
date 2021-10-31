00156dc7e12e64681d07f5b947adc7fd404fd1ea
maicumaicu <71650151+maicumaicu@users.noreply.github.com>
2021-10-31 15:20:57 -0300

    update

--- proyecto_tp_v3_master/proyecto_tp_v3_master.ino
+++ proyecto_tp_v3_master/proyecto_tp_v3_master.ino
@@ -1,39 +1,148 @@
-// Basic bluetooth test sketch. HC-06_01_9600
-// HC-06 ZS-040
-//
-//
-//  Uses hardware serial to talk to the host computer and software serial for communication with the bluetooth module
-//
-//  Pins
-//  BT VCC to Arduino 5V out.
-//  BT GND to GND
-//  BT RX to Arduino pin 3 (through a voltage divider)
-//  BT TX to Arduino pin 2 (no need voltage divider)
-//
-//  When a command is entered in the serial monitor on the computer
-//  the Arduino will relay it to the bluetooth module and display the result.
-//
-//  These HC-06 modules require capital letters and no line ending
-//
+#define CANT_BTN 2
+#define ESPERA 0
+#define CONFIRMACION 1
+#define LIBERACION 2
+#define TICKS_BTN  25
 
+#define PIERNA_DERECHA 0
+#define PIERNA_IZQUIERDA 1
+#define BRAZO_DERECHA 2
+#define BRAZO_IZQUIERDA 3
+#define PECHO 4
+
+#define ON 0
+#define RESULT 1
+#define START 2
+
+struct button {
+  public:
+    int pin;
+    int estado;
+    int flag;
+};
+button btns[CANT_BTN];
 #include <SoftwareSerial.h>
+#include <TimerOne.h>
 SoftwareSerial BTSerial(10, 11); // RX | TX
+int j, lecturaBtn, ticks, timer, part, ms;
+int choice;
+bool correct;
+int estadoIotino = START;
 
 void setup()
 {
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(tick);
   Serial.begin(9600);
   Serial.println("Arduino with HC-06 is ready");
 
   // HC-06 default baud rate is 9600
   BTSerial.begin(9600);
   Serial.println("BTserial started at 9600");
-  BTSerial.write('0');
+  for (int i = 2 , j = 0; j < CANT_BTN; j++, i++) {
+    btns[j].pin = i;
+    btns[j].estado = ESPERA;
+    btns[j].flag = 0;
+    pinMode(btns[j].pin, INPUT_PULLUP);
+  }
+  part = randomPartSelecter();
 }
 
 void loop()
 {
-  BTSerial.write('0');
-  delay(1000);
-  BTSerial.write('1');
-  delay(1000);
+  for (j = 0; j < CANT_BTN; j++) {
+    btnMachine(j);
+  }
+  IotinoMachine();
+}
+
+int randomPartSelecter() {
+  int part = random(0, CANT_BTN);
+  Serial.println(part);
+  return part;
+}
+
+void IotinoMachine() {
+  switch (estadoIotino) {
+
+    case START:
+
+      if (BTSerial.available()) {
+        estadoIotino = ON;
+      } else {
+        BTSerial.write(part);
+      }
+      break;
+    case ON:
+      for (int x = 0; x < CANT_BTN; x ++) {
+        if (btns[x].flag == 1) {
+          estadoIotino = RESULT;
+          choice = x;
+          if (part == choice) {
+            BTSerial.write('1');
+            Serial.print(0);
+          } else {
+            BTSerial.write('0');
+          }
+        }
+      }
+      ms = 0;
+      break;
+    case RESULT:
+      Serial.println(ms);
+      if (ms >= 1000) {
+        Serial.print(0);
+        BTSerial.write('2');
+      }
+      if (BTSerial.available()) {
+        char d = BTSerial.read();
+        Serial.print(d);
+        if (d == '1') {
+          Serial.print(BTSerial.read());
+          estadoIotino = START;
+          part = randomPartSelecter();
+        }
+      }
+
+      break;
+  }
+}
+
+
+void btnMachine(int index) {
+  switch (btns[index].estado) {
+    case ESPERA:
+      btns[index].flag = 0;
+      lecturaBtn = digitalRead(btns[index].pin);
+      if (lecturaBtn == 0) {
+        ticks = 0;
+        btns[index].estado = CONFIRMACION;
+      }
+      break;
+
+    case CONFIRMACION:
+      lecturaBtn = digitalRead(btns[index].pin);
+      if ( lecturaBtn == 0 && ticks >= TICKS_BTN) {
+        btns[index].estado = LIBERACION;
+      }
+
+      if ( lecturaBtn == 1 && ticks < TICKS_BTN) {
+        btns[index].estado = ESPERA;
+      }
+      break;
+
+    case LIBERACION:
+      lecturaBtn = digitalRead(btns[index].pin);
+      if ( lecturaBtn == 1) {
+        btns[index].flag = 1;
+        btns[index].estado = ESPERA;
+      }
+      break;
+  }
+}
+
+void tick() {
+  ticks++;
+  timer++;
+  ms++;
 }
--- proyecto_tp_v3_slave/proyecto_tp_v3_slave.ino
+++ proyecto_tp_v3_slave/proyecto_tp_v3_slave.ino
@@ -1,27 +1,38 @@
-// Basic Bluetooth sketch HC-05_03_9600
-// Connect the HC-05 module and communicate using the serial monitor
-//
-// The HC-05 defaults to commincation mode when first powered on.
-// Needs to be placed in to AT mode
-// After a factory reset the default baud rate for communication mode is 38400
-//
-//
-//  Pins
-//  BT VCC to Arduino 5V out.
-//  BT GND to GND
-//  BT RX to Arduino pin 3 (through a voltage divider)
-//  BT TX to Arduino pin 2 (no need voltage divider)
-
 
 #include <SoftwareSerial.h>
-SoftwareSerial BTserial(10, 11); // RX | TX
-// Connect the HC-05 TX to Arduino pin 2 RX.
-// Connect the HC-05 RX to Arduino pin 3 TX through a voltage divider.
+#include <Adafruit_NeoPixel.h>
+#include <TimerOne.h>
+#ifdef __AVR__
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
+#define ON         0
+#define GUESS      1
+#define RESULT     2
+
+#define CANT_BTN 2
+#define PIN_MATRIZ 12
+#define NUMPIXELS  64
+
+SoftwareSerial BTserial(10, 11); // RX | TX
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_MATRIZ, NEO_GRB + NEO_KHZ800);
+
+struct part {
+  public:
+    int r;
+    int g;
+    int b;
+};
+
+part body[CANT_BTN];
+int choice, screenState, ms;
+char d = ' ';
 char c = ' ';
 
 void setup()
 {
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(tick);
   // start the serial communication with the host computer
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
@@ -30,29 +41,114 @@ void setup()
   // start communication with the HC-05 using 9600
   BTserial.begin(9600);
   Serial.println("BTserial started at 9600");
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+  pixels.setBrightness(10);
+  for (int j = 0; j < CANT_BTN; j++) {
+    body[j].r = 255;
+    body[j].g = 0;
+    body[j].b = 0;
+  }
 }
 
 void loop()
 {
-  Serial.println(c);
-  // Keep reading from HC-05 and send to Arduino Serial Monitor
-  if (BTserial.available())
-  {
-    c = BTserial.read();
-    
-    if (c == '0') {
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if (c == '1') {
-      digitalWrite(LED_BUILTIN, LOW);
-    }
-  }
+  screenMachine();
 
-  // Keep reading from Arduino Serial Monitor and send to HC-05
-  if (Serial.available())
-  {
-    c =  Serial.read();
-    BTserial.write(c);
-  }
+}
 
+void screenMachine() {
+  switch (screenState) {
+    case ON:
+      pixels.clear();
+      pixels.show();
+      if (BTserial.available())
+      {
+        Serial.println(0);
+        c = BTserial.read();
+        for (int x = 0; x < CANT_BTN; x ++) {
+          if (c == x) {
+            prenderParte(body[c].r, body[c].g, body[c].b, c);
+            screenState = GUESS;
+            BTserial.write(1);
+            ms = 0;
+          }
+        }
+      }
+
+      break;
+    case GUESS:
+
+      if (BTserial.available()) {
+        c = BTserial.read();
+        Serial.println(c);
+      }
+      if (c == '1') {
+        Serial.println(3);
+        victoria();
+        screenState = RESULT;
+      } else if (c == '0') {
+        derrota();
+        Serial.println(3);
+        screenState = RESULT;
+      }
+
+      break;
+    case RESULT:
+      if (BTserial.available()) {
+        d = BTserial.read();
+        Serial.println(d);
+        if (d == '2') {
+          BTserial.write('1');
+          screenState = ON;
+          Serial.println(2);
+        }
+      } else {
+        if (c == '1') {
+          Serial.println(3);
+          victoria();
+        } else if (c == '0') {
+          derrota();
+          Serial.println(3);
+        }
+      }
+
+
+      break;
+  }
+}
+
+void prenderParte(int red, int green, int blue, int parte) {
+  for (int x = parte; x < NUMPIXELS; x += 8) {
+    pixels.setPixelColor(x, pixels.Color(red, green, blue));
+    pixels.show();
+  }
+}
+
+void victoria() {
+  for (int x = 0; x < NUMPIXELS; x ++) {
+    if (x == 1) {
+      pixels.setPixelColor(0, pixels.Color(0, 125, 0));
+    } else if (x == 2) {
+      pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    } else if (x == 0) {
+      pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+    }
+    pixels.setPixelColor(x + 2, pixels.Color(0, 255, 0));
+    pixels.setPixelColor(x + 1, pixels.Color(0, 125, 0));
+    pixels.setPixelColor(x, pixels.Color(0, 0, 0));
+    pixels.show();
+  }
+}
+
+void derrota() {
+  for (int x = 0; x < NUMPIXELS; x ++) {
+    pixels.setPixelColor(x, pixels.Color(255, 0, 0));
+    pixels.show();
+  }
+}
+
+void tick() {
+  ms++;
 }
